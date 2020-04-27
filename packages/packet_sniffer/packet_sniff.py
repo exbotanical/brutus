065d7f7a4a0caf53fabcd0e153c953b194e02bac
Goldmund <matthewtzito@gmail.com>
2020-04-27 11:53:10 -0700

    update scapy

--- packages/packet_sniffer/packet_sniff.py
+++ packages/packet_sniffer/packet_sniff.py
@@ -10,7 +10,7 @@ wireless device, this program will sniff HTTP traffic
 and output URLs and possible user credentials.
 """
 import scapy.all as scapy
-from scapy.layers import http
+from scapy.layers.http import *
 
 class Sniffer:
     def __init__(self, interface):
@@ -39,11 +39,11 @@ class Sniffer:
         if (packet.haslayer(http.HTTPRequest)): 
             url = self.get_url(packet)
             print("[+] HTTP Request")
-            print(url + "\n")
+            print(url.decode() + "\n")
             credentials = self.get_credentials(packet)
             if (credentials):
                 print("\n[+] Prospective credentials found")
-                print(credentials + "\n")
+                print(str(credentials) + "\n")
             
 
     def sniff(self, interface):
